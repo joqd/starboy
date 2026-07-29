fbf9ff446bce7493b7d8519475f64570ee5f5b77
joqd <gorgdis@gmail.com>
2026-07-29 17:56:48 +0330

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,36 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
-
+import Image from "next/image"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
-
-const fontMono = Geist_Mono({
-    subsets: ["latin"],
-    variable: "--font-mono",
-})
-
-export default function RootLayout({
-    children,
-}: Readonly<{
+type RootLayoutProps = {
     children: React.ReactNode
-}>) {
+}
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
     return (
-        <html
-            lang="en"
-            suppressHydrationWarning
-            className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
-        >
-            <body>
-                <ThemeProvider>{children}</ThemeProvider>
+        <html lang="en" suppressHydrationWarning className={cn("antialiased", "font-sans")}>
+            <body className="relative h-screen w-screen overflow-hidden">
+                <ThemeProvider>
+                    {children}
+                    <div className="absolute bottom-0 left-0">
+                        <Image
+                            src="/images/baby-blue-movie.jpg"
+                            alt="Starboy"
+                            width={657}
+                            height={841}
+                            className="hidden w-64 object-contain select-none md:w-100 lg:w-120 dark:block"
+                        />
+
+                        <Image
+                            src="/images/baby-blue-movie-light.jpg"
+                            alt="Starboy"
+                            width={657}
+                            height={841}
+                            className="w-64 object-contain select-none md:w-100 lg:w-120 dark:hidden"
+                        />
+                    </div>
+                </ThemeProvider>
             </body>
         </html>
     )
