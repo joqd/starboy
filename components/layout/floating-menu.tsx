71cbd9549ce8a8e0bec09455936131fe26e0687e
joqd <gorgdis@gmail.com>
2026-08-05 21:50:31 +0330

    fix: remove unnecessary class from Separator component in FloatingMenu

--- components/layout/floating-menu.tsx
+++ components/layout/floating-menu.tsx
@@ -49,7 +49,7 @@ function MenuItems() {
             <Cart />
             <ModeToggle />
 
-            <Separator className="" orientation="vertical" />
+            <Separator orientation="vertical" />
             <LoginDialog />
         </div>
     )
