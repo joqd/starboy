011cda2baeecaf20e895fdadf23ff49e6ef8e0d9
joqd <gorgdis@gmail.com>
2026-08-05 13:14:34 +0330

    fix: update API URL handling in next.config.ts for improved flexibility

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next"
 
+const apiUrl = new URL(process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000")
+
 const nextConfig: NextConfig = {
     output: "standalone",
     images: {
+        dangerouslyAllowLocalIP: process.env.NODE_ENV === "development",
         unoptimized: process.env.NODE_ENV === "development",
-		qualities: [75, 95],
+        qualities: [75, 95],
         remotePatterns: [
             {
-                protocol: "http",
-                hostname: "localhost",
+                protocol: apiUrl.protocol.replace(":", "") as "http" | "https",
+                hostname: apiUrl.hostname,
+                port: apiUrl.port,
             },
         ],
     },
