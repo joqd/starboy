a60c69a5217a1c3f1eb293c7a13d661e9e01e846
joqd <gorgdis@gmail.com>
2026-08-05 12:35:47 +0330

    fix: correct image qualities configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     output: "standalone",
     images: {
         unoptimized: process.env.NODE_ENV === "development",
+		qualities: [75, 95],
         remotePatterns: [
             {
                 protocol: "http",
