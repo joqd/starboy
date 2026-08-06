14090ab973fc9e19e6b415ce89e6281b8870699e
joqd <gorgdis@gmail.com>
2026-08-06 21:26:22 +0330

    feat: add redirect from /p to home in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,15 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    async redirects() {
+        return [
+            {
+                source: "/p",
+                destination: "/",
+                permanent: true,
+            },
+        ]
+    },
 }
 
 export default nextConfig
