4471693c442621781924afdf472f4fbe2e1d4dc6
joqd <gorgdis@gmail.com>
2026-08-14 18:21:35 +0330

    feat: refactor floating menu to include BlogButton and improve layout; remove unused MenuItems component

--- components/layout/floating-menu.tsx
+++ components/layout/floating-menu.tsx
@@ -4,10 +4,16 @@ import { Music } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
 import { ModeToggle } from "@/components/layout/mode-toggle"
 import { LoginDialog } from "@/components/user/login-dialog"
+import { Rss } from "lucide-react"
 import Cart from "@/components/cart/cart"
 import { Button } from "@/components/ui/button"
 import { useAudio } from "@/hooks/use-audio"
 import { cn } from "@/lib/utils"
+import Link from "next/link"
+
+function Space() {
+    return <div className="w-1"></div>
+}
 
 function PlayRandomButton() {
     const { currentAudio, random, setPlaying } = useAudio()
@@ -20,33 +26,35 @@ function PlayRandomButton() {
     const isVisible = !currentAudio
 
     return (
-        <div
-            className={cn(
-                "flex transition-all duration-300",
-                isVisible
-                    ? "w-9 scale-100 opacity-100"
-                    : "pointer-events-none w-0 scale-75 opacity-0"
-            )}
-        >
-            <Button variant="ghost" size="icon" onClick={handleClick} aria-label="پخش موزیک تصادفی">
-                <Music className="h-[1.2rem] w-[1.2rem]" />
-            </Button>
+        <div>
+            <div
+                className={cn(
+                    "flex transition-all duration-300",
+                    isVisible
+                        ? "w-9 scale-100 opacity-100"
+                        : "pointer-events-none w-0 scale-75 opacity-0"
+                )}
+            >
+                <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={handleClick}
+                    aria-label="پخش موزیک تصادفی"
+                >
+                    <Music className="h-[1.2rem] w-[1.2rem]" />
+                </Button>
+            </div>
         </div>
     )
 }
 
-function MenuItems() {
+function BlogButton() {
     return (
-        <div className="flex items-center rounded-xl bg-accent px-2 py-1">
-            <div className="hidden lg:block">
-                <PlayRandomButton />
-            </div>
-            <Cart />
-            <ModeToggle />
-
-            <Separator className="mx-1" orientation="vertical" />
-            <LoginDialog />
-        </div>
+        <Link href={"/blog"}>
+            <Button variant={"ghost"} className={"cursor-pointer"}>
+                <Rss className="h-[1.2rem] w-[1.2rem]" />
+            </Button>
+        </Link>
     )
 }
 
@@ -55,14 +63,25 @@ export default function FloatingMenu() {
         <div>
             <div className="fixed right-0 bottom-6 z-9999 hidden px-8 lg:block">
                 <div className="flex items-center">
-                    <MenuItems />
+                    <div className="flex items-center gap-1 rounded-xl bg-accent px-2 py-1">
+                        {/* <PlayRandomButton />  */}
+                        <Cart />
+                        <BlogButton />
+                        <ModeToggle />
+                        <LoginDialog />
+                    </div>
                 </div>
             </div>
 
             <div dir="rtl" className="fixed top-0 left-0 z-9999 w-full lg:hidden">
-                <div className="flex items-center justify-between border-white/10 px-5 py-3">
-                    <div dir="ltr">
-                        <MenuItems />
+                <div className="mx-3 my-2 flex items-center justify-between rounded-2xl border border-background/40 bg-accent/80 px-2 py-1 backdrop-blur-xl dark:border-none">
+                    <div className="flex items-center">
+                        <Cart />
+                        <BlogButton />
+                        <ModeToggle />
+                    </div>
+                    <div className="flex items-center">
+                        <LoginDialog />
                     </div>
                 </div>
             </div>
