f5eb21a67dffea5610eea4f9c14e6083354d9434
joqd <gorgdis@gmail.com>
2026-09-04 15:03:48 +0330

    feat: create OrdersPage component with basic layout

---
+++ app/(main)/orders/page.tsx
@@ -0,0 +1,11 @@
+import { PageContainer } from "@/components/layout/page-container"
+
+export default function OrdersPage() {
+    return (
+        <PageContainer>
+            <main dir="rtl" className="min-h-screen">
+				This is order page
+			</main>
+        </PageContainer>
+    )
+}
